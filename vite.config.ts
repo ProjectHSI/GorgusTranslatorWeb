9afbe3db2a459a4980406272facbb116547e3194
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2025-05-12 12:22:01 +1000

    reorder compileTime()

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import compileTime from "vite-plugin-compile-time"
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-	plugins: [topLevelAwait(), sveltekit(), compileTime()],
+	plugins: [compileTime(), topLevelAwait(), sveltekit()],
 
 	/*test: {
 		include: ['src/!**!/!*.{test,spec}.{js,ts}']
