cfd1fb1537828b3d0c59f656d167358ae97efcfa
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2025-05-13 16:53:53 +1000

    fix download_pip.ps1

--- download_pip.ps1
+++ download_pip.ps1
@@ -1,5 +1,7 @@
 #pip install -U pip setuptools wheel
 #pip install -r ./Gorgus-Translator/requirements.txt
 
-Remove-Item ./src/lib/python/pip_libs/
+try {
+    Remove-Item ./src/lib/python/pip_libs/ -Recurse -Force -Confirm:$false;
+} catch { }
 pip download -d ./src/lib/python/pip_libs/ -r ./Gorgus-Translator/requirements.txt
