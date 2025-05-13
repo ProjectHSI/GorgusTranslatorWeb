5f490a5e79a967292f0e5d411eb0e940fb3cc948
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2025-05-13 16:32:22 +1000

    remove existing pip libraries in download_pip.ps1

--- download_pip.ps1
+++ download_pip.ps1
@@ -1,3 +1,5 @@
 #pip install -U pip setuptools wheel
 #pip install -r ./Gorgus-Translator/requirements.txt
+
+Remove-Item ./src/lib/python/pip_libs/
 pip download -d ./src/lib/python/pip_libs/ -r ./Gorgus-Translator/requirements.txt
